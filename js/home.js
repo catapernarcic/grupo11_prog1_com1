f4e832ff668d67ec9b089eecb6c8c2dfd86ffdc8
teodelinalm <tlagosmarmol@udesa.edu.ar>
2024-11-15 19:56:46 -0300

    java del home y algunos detalles

--- js/home.js
+++ js/home.js
@@ -1,18 +1,23 @@
-fetch('')
+let section_main = document.querySelector(".section_main")
+let url = 'https:/dummyjson.com/recipes'
+
+console.log(section_main)
+
+fetch(url)
 .then(function(response) {
   return response.json()
 })
 .then(function(data) {
   console.log(data);
   let characters = "";
-  for (let i = 0; i < data.results.length; i++){
-
+  for (let i = 0; i < data.results.length; i++){ // da error algo del length
+    results +=    
 
     `<article>
-    <img src="" alt="">
-    <h2></h2>
-    <p>Nivel de dificultad: </p>
-    <p> <a href="./detalle_receta.html">Detalle</a></p>
+    <img src=${data.results[i].image} alt="imagen">
+    <h2>${data.results[i].name}</h2>
+    <p>Nivel de dificultad: ${data.results[i].difficulty} </p>
+    <p> <a href="./detalle_receta.html?id=${data.results[i].id}">Detalle</a></p>
     </article>`
 
   };
