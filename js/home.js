fda3f77cc4a86c410a9b3ea43ecc89277367dafe
catapernarcic <cpernarcic@udesa.edu.ar>
2024-11-26 12:40:06 -0300

    corregí boton para que funcione (java), y hice css de las nuevas recetas

--- js/home.js
+++ js/home.js
@@ -19,7 +19,7 @@ fetch(url)
     <img src=${data.recipes[i].image} alt="imagen" class="img_receta">
     <h2>${data.recipes[i].name}</h2>
     <p>Nivel de dificultad: ${data.recipes[i].difficulty} </p>
-    <p class="data-recipie"> <a href="./detalle_receta.html?id=${data.recipes[i].id}">Detalle</a></p>
+    <p class="data-recipie"> <a href="./detalle_receta.html?id=${data.recipes[i].id}">Ir a detalle</a></p>
     </article>`;
     section_main.innerHTML += characters;
   };
@@ -30,38 +30,38 @@ fetch(url)
 })
 }
 
+  let boton = document.querySelector("#cargar");
+  let section = document.querySelector(".section_main");
+  let pagina = 0; 
 
-let boton = document.querySelector("#cargar")
-let pagina = 0
-
-boton.addEventListener("click", function(){
-    console.log(pagina);
-    pagina +=10
-    console.log(pagina);
-
-    fetch(`https://dummyjson.com/recipes?limit=10&skip=${pagina}`)
-      .then(function(res){
-       return res.json()
-    })
-    .then(function(data){
-        console.log(data)
-
-
-        for(let i = 0; i < 10; i++){
-            section.innerHTML += `
-                <article>
-                    <img src="${data.recipes[i].image}" alt="">
-                    <div>
-                        <h3>${data.recipes[i].name}</h3>
-                        <p>${data.recipes[i].difficulty}</p>
-                        <a href="detalle-receta.html?id=${data.recipes[i].id}">Ir a detalle</a>
-                    </div>
-                </article>
-            `
-        }
-    })
-})
-
-
-
- 
+  boton.addEventListener("click", function () {
+      pagina += 10;
+      console.log("Cargando recetas desde la página:", pagina);
+      fetch(`https://dummyjson.com/recipes?limit=10&skip=${pagina}`)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data) {
+              
+              console.log(data); 
+              if (data.recipes && data.recipes.length > 0) {
+                  data.recipes.forEach(recipe => {
+                      section.innerHTML += `
+                          <article class="art_receta">
+                              <img class="img_receta" src="${recipe.image}" alt="${recipe.name}">
+                              <h3>${recipe.name}</h3>
+                              <p>Dificultad: ${recipe.difficulty}</p>
+                              <a class="data-recipie" href="detalle_receta.html?id=${recipe.id}">Ir a detalle</a>
+                          </article>
+                      `;
+                  });
+                  pagina += 10;
+              } else {
+                console.log("No hay más recetas para cargar.");
+                boton.style.display = "none";
+            }
+          })
+          .catch(error => {
+              console.error("Error al cargar recetas:", error);
+          });
+  });
