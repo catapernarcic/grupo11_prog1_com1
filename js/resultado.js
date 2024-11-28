fd6b213642783ec30c08309e23c374db4a112509
teodelinalm <tlagosmarmol@udesa.edu.ar>
2024-11-28 11:02:39 -0300

    arregle un par de cosas del js de resultados

--- js/resultado.js
+++ js/resultado.js
@@ -10,7 +10,7 @@ console.log("Valor de q:", q);
 
 let url = `https://dummyjson.com/recipes/search?q=${q}`
 
-
+let resultado_b = document.querySelector(".resultado_de_busqueda")
 
 fetch(url)
 .then(function(response) {
@@ -19,20 +19,29 @@ fetch(url)
 .then(function(data) {
     console.log(data);
     
-    let results = "";
-    for (let i = 0; i < data.recipes.length; i++){
+    if (data.recipes.length == 0){
+      resultado_b.innerHTML = `No se encontraron resultados para: ${q}`
+    }
+    else{
+      resultado_b.innerHTML = `Resultado de busqueda para ${q}`
+      let results = "";
+      for (let i = 0; i < data.recipes.length; i++){
         results +=
-    
-        `<article class="art_receta">
+
+        `
+        <article class="art_receta">
         <img src=${data.recipes[i].image} alt="imagen" class="img_receta">
         <h2>${data.recipes[i].name}</h2>
         <p>Nivel de dificultad: ${data.recipes[i].difficulty} </p>
         <p> <a href="./detalle_receta.html?id=${data.recipes[i].id}">Detalle</a></p>
         </article>`;
     
-      };     
-
+      };  
+       
+    
     search_results.innerHTML = results; 
+    }
+
 })
 .catch(function(error) {
     console.log("Error: " + error);
