b927044b9f2861bb85abd7950a7a5844dff34a85
milaagudo1 <lagudomarrodan@udesa.edu.ar>
2024-11-28 15:42:26 -0300

    css

--- js/registro.js
+++ js/registro.js
@@ -1,38 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("registerForm");
+let form = document.querySelector("#registerForm");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
   
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
-  
-      const email = document.getElementById("email").value.trim();
-      const password = document.getElementById("password").value.trim();
-  
-      
-      const emailError = document.getElementById("emailError");
-      const passwordError = document.getElementById("passwordError");
-  
-      let isValid = true;
+    let email = document.querySelector("#email");
+    let password = document.querySelector("#password");
   
       
-      if (!email) {
-        emailError.textContent = "Por favor complete el campo";
-        isValid = false;
-      } else {
-        emailError.textContent = "";
-      }
+    let emailError = document.querySelector("#emailError");
+    let passwordError = document.querySelector("#passwordError");
+  
+    let isValid = true;
   
       
-      if (!password) {
-        passwordError.textContent = "Por favor complete el campo";
-        isValid = false;
-      } else {
-        passwordError.textContent = "";
-      }
+    if (email == "") {
+      emailError.innerHTML = "Por favor complete el campo";
+      isValid = false;
+    }
+  
+      
+    if (password == "") {
+      passwordError.innerHTML = "Por favor complete el campo";
+      isValid = false;
+    } 
   
      
-      if (isValid) {
-        window.location.href = "login.html";
-      }
-    });
-  });
-  
+    if (isValid) {
+      window.location.href = "login.html";
+    }
+});
+
